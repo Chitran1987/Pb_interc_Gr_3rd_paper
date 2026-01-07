610e314f9a58a8f0fc8c4bd3bb9be0a0618c8290
Chitran1987 <ghosal.chitran@gmail.com>
2026-01-08 00:20:59 +0100

    Updated trash

--- trash.R
+++ trash.R
@@ -4,7 +4,7 @@ library(ImageMetrologyAnalysis)
 
 #####################################1st image#################################################
 load(file = 'FFT_map_gr_STM_img_Jan_24_13.40.14_2023_[6-1]_1.0V_100pA.rda') #Original Image
-load(file = 'STM_img_Jan_24_13.40.14_2023_[6-1]_1.0V_100pA.rda') #Graphene map (redo with fewer spots)
+load(file = 'STM_img_Jan_24_13.40.14_2023_[6-1]_1.0V_100pA.rda') #Graphene
 load(file = 'FFT_map_STM_img_Jan_24_13.40.14_2023_[6-1]_1.0V_100pA.rda') #SiC map
 #####################################1st image#################################################
 
@@ -22,11 +22,13 @@ load(file = 'FFT_map_STM_img_Jan24_15.18.12_2023_[6-8]_10mV_100pA.rda') #R3G map
 #####################################4th image (large image) #################################################
 load(file = "STM_img_Tue_Jan24_13.23.39_2023_[5-1]_1.0V_100pA.rda") #Original image
 load(file = "FFT_map_STM_img_Jan24_13.23.39_2023_[5-1]_1.0V_100pA.rda") #SiC map
+load(file = 'FFT_map_gr_STM_img_Tue_Jan24_13.23.39_2023_[5-1]_1.0V_100pA.rda') #Gr map
 ##Do a map of graphene and R3G if possible
 #####################################4th image (large image) #################################################
 
 
 #Plotting
-plot2D.arr(arr = map.img.large)
+plot2D.arr(arr = img.large)
+plot2D.arr(arr = map.img.large.gr)
 
 
