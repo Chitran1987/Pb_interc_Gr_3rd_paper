8cd247bbd3208487ff2a2dce28f22c38b869d3ce
Chitran1987 <ghosal.chitran@gmail.com>
2026-01-07 13:59:35 +0100

    large_fft_map.R changed

--- large_fft_map.R
+++ large_fft_map.R
@@ -14,7 +14,7 @@ f_img.large <- plot2D.zoom(arr = f_img.large, center = c(0,0), Del_X = 80, Del_Y
 
 ###Take out the (0,0) spots
 ##plot2D boxes around the (0,0) spots
-v <- c(-1,-1,1,1)
+v <- c(-7.5,-7.5,7.5,7.5)
 v.mat <- matrix(data = v, ncol = 4, byrow = T)
 dmp <- plot2D.boxes(f_img.large, box.mat = v.mat, box_intens = 0.1, box.thick = 0.1)
 ##mask out the box
@@ -38,8 +38,10 @@ k0.vec <- vec.zer
 
 #Decide the Del_X and Del_Y for the mapping
 plot2D.arr(arr = img.large)
-dmp <- plot2D.zoom(arr = img.large, center = c(11,7.5), Del_X = 1, Del_Y = 1)
+dmp <- plot2D.win.sig(tens = img.large, center=c(11, 10), Xspan = 1, Yspan = 1, k=10)
 f.dmp <- fft_2D(tens = dmp, pl='amp')
+f.dmp <- plot2D.zoom(f.dmp[[1]], center = c(0,0), Del_X = 60, Del_Y = 60)
+f.dmp <- mask.arr.box(tens = f.dmp, box.vec = c(-7.5,-7.5,7.5,7.5))
 
 ###Call the fft_2D_map() function
 map.img.large <- fft_2D_map(img.tens = img.large, DelX = 1, DelY = 1, k1st = b.mat, k0 = k0.vec)
