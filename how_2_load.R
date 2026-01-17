724205e920681a263e6ed6193b71e0b163d587da
Chitran1987 <ghosal.chitran@gmail.com>
2026-01-18 00:09:34 +0100

    saved a new .rda for the 500mV image

--- how_2_load.R
+++ how_2_load.R
@@ -11,3 +11,7 @@ save(img.gr.sm, file = 'STM_img_Jan24_14.50.13_2023_[6-6]_70mV_100pA.rda')
 #Small R3G specific image
 img.R3G.sm <- img_2_arr(source.png = 'm1_Z RetraceUp Tue Jan 24 15.18.12 2023 [6-8]_10mV_100pA.png', x.lim = c(0, 6.115), y.lim = c(0, 6.115))
 save(img.R3G.sm, file = 'STM_img_Jan24_15.18.12_2023_[6-8]_10mV_100pA.rda')
+
+#The 500mV image
+img.500mV <- img_2_arr(source.png = 'm1_Z RetraceDown Tue Jan 24 14.08.14 2023 [6-3] _0.5V_100pA.png', x.lim = c(0, 6.115), y.lim = c(0, 6.115))
+save(img.500mV, file = 'STM_img_Jan24_14.08.14_2023_[6-3] _0.5V_100pA.rda')
